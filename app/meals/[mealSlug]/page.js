d50a07b53a601dafef15f551abfa0487ff864973
Naser <108549635+naser0o0@users.noreply.github.com>
2024-01-18 15:18:31 +0100

    meals detail & from & imapge picker

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,38 @@
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
 
+import classes from "./page.module.css";
 
-export default function MealDetailsPage({params}) {
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+  if(!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
   return (
-    <div>
-        <h1>Meal Details</h1>
-    </div>
-  )
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        {/* In React, dangerouslySetInnerHTML is a prop that allows you to render raw HTML content within a component.  */}
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
